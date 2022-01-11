747998b4a0c7ef6671b72a458c0838cd6c57683d
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-11 21:45:19 +0530

    Rabbitmq Config
    Signed-off-by: Madhu427 <madhusudhanachary.k@gmail.com>

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -66,3 +66,36 @@ STAT_CHECK $? "Updated redis"
 systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
 STAT_CHECK $? "Start Redis"
 
+
+#Erlang is a dependency which is needed for RabbitMQ.
+## yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+#Setup YUM repositories for RabbitMQ.
+## curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+#Install RabbitMQ
+## yum install rabbitmq-server -y
+#Start RabbitMQ
+## systemctl enable rabbitmq-server
+## systemctl start rabbitmq-server
+#RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence we need to create one user for the application.
+#
+#Create application user
+## rabbitmqctl add_user roboshop roboshop123
+## rabbitmqctl set_user_tags roboshop administrator
+## rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo -e "\e[1;33m----------------> RABBITMQ SETUP<--------------\e[0m"
+
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>${LOG_FILE}
+STAT_CHECK $? "Erlang dependency"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
+STAT_CHECK $? "Yum Repository setup"
+
+yum install rabbitmq-server -y &>>{LOG_FILE}
+STAT_CHECK $? "Rabbit MQ installed"
+
+systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
+STAT_CHECK $? "Start Rabbit MQ"
+
+
+
