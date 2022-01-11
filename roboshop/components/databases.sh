f688a4fec5a9e09fed1e2f427d44f1f4a3f4a6da
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-11 20:29:17 +0530

    adding source to databases
    Signed-off-by: Madhu427 <madhusudhanachary.k@gmail.com>

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -20,6 +20,8 @@
 ## mongo < catalogue.js
 ## mongo < users.js
 
+source components/common.sh
+
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 STAT_CHECK $? "Mongodb Repo file download"
 
@@ -29,7 +31,7 @@ STAT_CHECK $? "Mongdb Installation"
 systemctl enable mongod &>>${LOG_FILE} && systemctl start mongod &>>${LOG_FILE}
 STAT_CHECK $? "Start mongodb"
 
-sed -i "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>${LOG_FILE}
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf &>>${LOG_FILE}
 STAT_CHECK $? "Update mongodb"
 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
