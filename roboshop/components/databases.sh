2ac297458c97774a39d76ec8c5e8ff5f61ee6586
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-13 01:20:08 +0530

    databases -- redis & mongodb
    Signed-off-by: Madhu427 <madhusudhanachary.k@gmail.com>

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -28,7 +28,7 @@ STAT_CHECK $? "Mongodb Repo file download"
 yum install -y mongodb-org  &>>${LOG_FILE}
 STAT_CHECK $? "Mongdb Installation"
 
-systemctl enable mongod &>>${LOG_FILE} && systemctl start mongod &>>${LOG_FILE}
+systemctl enable mongod &>>${LOG_FILE} && systemctl restart mongod &>>${LOG_FILE}
 STAT_CHECK $? "Start mongodb"
 
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf &>>${LOG_FILE}
