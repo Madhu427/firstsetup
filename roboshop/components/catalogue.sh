3bd29df17d94beb061a679bd3d762e3d14e6983f
Madhu427 <madhusudhanachary.k@gmail.com>
2022-01-12 16:29:00 +0530

    catalogue config
    Signed-off-by: Madhu427 <madhusudhanachary.k@gmail.com>

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -40,7 +40,7 @@ DOWNLOAD catalogue
 rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cp -r /tmp/catalogue-main/*  /home/roboshop/catalogue &>>${LOG_FILE}
 STAT_CHECK $? "Copy catalogue content"
 
-cd /home/roboshop/catalogue && npm install &>>${LOG_FILE}
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>${LOG_FILE}
 STAT_CHECK $? "npm installed"
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
